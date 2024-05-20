86f5f21082f4171e0cadde5b108052ea1e7dd0fe
DiegoGarV <dgv3105@gmail.com>
2024-05-19 23:36:57 -0600

    Regreso

--- CadenasMarkov.py
+++ CadenasMarkov.py
@@ -1,16 +1,10 @@
-
 # Menganita y Chispudito juegan con una moneda balanceada (no cargada). El juego consiste en que cada uno escoge una combinación de caras y cruces de largo 3 y empiezan a tirar la moneda. 
 # La primera persona que obtenga su combinación al tirar la moneda gana (la moneda se tira hasta que alguno de los dos gane). 
 # Menganita selecciona CCC, mientras que Chispudito selecciona XCX. 
   
-# Para ayudarle a Menganita, debe calcular la probabilidad de que cada uno de ellos gane.
-
+# Para ayudarle a Menganita, debe calcular la probabilidad de que cada uno de ellos gane.
 import numpy as np
-
-
 # Definimos la matriz de transición
-print("PROBLEMA RESOLVIENDOLO UTILIZANDO CADENAS DE MARKOV")
-
 P = np.array([[1,0, 0,  0,  0,  0,  0,  0 ],
             [0.5, 0, 0,  0,  0.5, 0,  0,  0 ],
             [0, 0,  0,  0.5, 0,  0,  0,  0.5],
@@ -22,15 +16,20 @@ P = np.array([[1,0, 0,  0,  0,  0,  0,  0 ],
 
 P2 = np.linalg.matrix_power(P,2)
 
+t = 0
 prob_menganita = 0
 prob_chispudito = 0
 for i in range(8):
     for j in range(8):
+        t += P2[i][j]
         if j == 0:
             prob_menganita += P2[i][j]
         if j == 6:
             prob_chispudito += P2[i][j]
 
-print(f"Probabilidad de que gane Menganita: {prob_menganita/8}")
-print(f"Probabilidad de que gane Chispudito: {prob_chispudito/8}")
+total = ((prob_chispudito + prob_menganita))
+print(t, total)
+
+print(f"Probabilidad de que gane Menganita: {(prob_menganita/8)*t/total}")
+print(f"Probabilidad de que gane Chispudito: {(prob_chispudito/8)*t/total}")
 
