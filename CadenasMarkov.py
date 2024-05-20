c85d33507c5c2965373c806e393ed72203753078
DiegoGarV <dgv3105@gmail.com>
2024-05-19 23:23:27 -0600

    terminado

--- CadenasMarkov.py
+++ CadenasMarkov.py
@@ -20,30 +20,17 @@ P = np.array([[1,0, 0,  0,  0,  0,  0,  0 ],
             [0, 0,  0,  0,  0,  0,  1,  0, ],
             [0, 0,  0,  0.5, 0,  0,  0,  0.5]])
 
-#Submatriz de estados transitorios (los que no son CCC Y XCX) 
-Q = P[np.ix_([1,2,3,4,5,7], [1,2,3,4,5,7])]
-# print("\n\n\n\n\n")
-# print(Q)
+P2 = np.linalg.matrix_power(P,2)
 
-#Submatriz de estados absorbentes (los que sí son CCC Y XCX) 
-R = P[np.ix_([1,2,3,4,5,7], [0,6])]
-# print("\n\n\n\n\n")
-# print(R)
+prob_menganita = 0
+prob_chispudito = 0
+for i in range(8):
+    for j in range(8):
+        if j == 0:
+            prob_menganita += P2[i][j]
+        if j == 6:
+            prob_chispudito += P2[i][j]
 
-#Matriz fundamental que se crea con la inversa de la resta de la submatriz Q a la matriz identidad 6x6. (N:=(I−Q)^−1)
-N = np.linalg.inv(np.eye(6) - Q)
-# print("\n\n\n\n\n")
-# print(N)
-
-#Producto escalar entre la matriz fundamental (N) y la sumatriz de estados absorbentes
-B = np.dot(N, R)
-# print("\n\n\n\n\n")
-# print(B)
-# print("\n\n\n\n\n")
-
-prob_menganita = B[0, 0]
-prob_chispudito = B[0,1]
-
-print(f"Probabilidad de que gane Menganita: {prob_menganita}")
-print(f"Probabilidad de que gane Chispudito: {prob_chispudito}")
+print(f"Probabilidad de que gane Menganita: {prob_menganita/8}")
+print(f"Probabilidad de que gane Chispudito: {prob_chispudito/8}")
 
